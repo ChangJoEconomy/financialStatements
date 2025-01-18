d2b1972c371c436db6e9563b4422218c0ffe5060
ChangJo <47021837+ChangJoEconomy@users.noreply.github.com>
2025-01-18 17:36:49 +0900

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,9 @@
+from getIndexInfo import get_index_info
+from getStockList import get_stocks_list
+
+def main():
+    get_index_info()
+    get_stocks_list()
+
+if __name__ == "__main__":
+    main()
