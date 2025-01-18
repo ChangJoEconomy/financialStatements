44d6ef1bd693f0b2de8003d7f9c2a17d3fec4148
ChangJo <47021837+ChangJoEconomy@users.noreply.github.com>
2025-01-19 01:00:29 +0900

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,7 +2,9 @@ from getIndexInfo import get_index_info
 from getStockList import get_stocks_list
 
 def main():
+    print("index 정보 불러오는 중...")
     get_index_info()
+    print("주식 리스트 불러오는 중...")
     get_stocks_list()
 
 if __name__ == "__main__":
